7fffb9c8d7488624a330013f2e428082e941c01c
Paul Yestchick <paul.yestchick@gmail.com>
2023-04-25 14:00:55 +0300

    redmine #25321

--- OPP.PDF.View/OPP.Help.View.Helper.pas
+++ OPP.PDF.View/OPP.Help.View.Helper.pas
@@ -10,7 +10,7 @@ uses
 type
   TOPPHelpViewHelper = class helper for TdxPDFViewer
   public
-    procedure RunPredicate(APredicate: TOPPHelpPredicate); overload;
+    procedure RunConcretePredicate(APredicate: TOPPHelpPredicate; ALevel: Integer; completion: TOPPHelpViewPredicateExecutionCompletion); overload;
     procedure RunPredicate(APredicate: TOPPHelpPredicate; ALevel: Integer; completion: TOPPHelpViewPredicateExecutionCompletion); overload;
   end;
 
@@ -20,7 +20,7 @@ uses
   System.SysUtils;
 
 const
-  kEventFlowName: String = 'dxPDFViewer';
+  kContext: String = 'dxPDFViewer';
 
 resourcestring
   SDebugStartedPredicateExecutionTemplate = 'Started predicate execution: %s';
@@ -28,7 +28,7 @@ resourcestring
 
   { TOPPHelpViewHelper }
 
-procedure TOPPHelpViewHelper.RunPredicate(APredicate: TOPPHelpPredicate);
+procedure TOPPHelpViewHelper.RunConcretePredicate(APredicate: TOPPHelpPredicate; ALevel: Integer; completion: TOPPHelpViewPredicateExecutionCompletion);
 var
   fCurrentPageIndex: Integer;
   fSearchResult: TdxPDFDocumentTextSearchResult;
@@ -38,46 +38,75 @@ begin
     ktSearch:
       begin
         fCurrentPageIndex := self.CurrentPageIndex;
+{$REGION 'DevEx feature fix'}
+          {the hack: devEx keeps previously searched expression in memory}
+          {so, in order to repeat the search with the same expression you have to start another search with fake data, i.e. with ' 'space}
+          {or, you can start searching using the the very first symbol of your expression, then, continue serarching using the the remaining characters of your expression}
+          {the fix should be applied in dxPDFDocument.pas: class TdxPDFDocumentSequentialTextSearch. method InternalClear}
+          {i.e: FWordIndex := -1;}
+          self.Document.FindText(' ',TdxPDFDocumentTextSearchOptions.Default, fCurrentPageIndex);
+{$ENDREGION}
         fSearchResult := self.Document.FindText(APredicate.value, TdxPDFDocumentTextSearchOptions.Default, fCurrentPageIndex);
         self.CurrentPageIndex := fSearchResult.range.pageIndex;
+        if assigned(completion) then
+          completion(perSuccess, ALevel);
       end;
     ktBookmark:
       begin
-        //
+        if assigned(completion) then
+          completion(perSuccess, ALevel);
       end;
     ktPage:
       begin
         self.CurrentPageIndex := StrToInt(APredicate.value);
+        if assigned(completion) then
+          completion(perSuccess, ALevel);
       end;
     ktAny:
       begin
-        //
+        if assigned(completion) then
+          completion(perSuccess, ALevel);
       end;
   end;
 
 end;
 
 procedure TOPPHelpViewHelper.RunPredicate(APredicate: TOPPHelpPredicate; ALevel: Integer; completion: TOPPHelpViewPredicateExecutionCompletion);
-var
-  nestedPredicate: TOPPHelpPredicate;
 begin
 
-  eventLogger.Flow(Format(SDebugStartedPredicateExecutionTemplate, [APredicate.asString]), kEventFlowName);
+  eventLogger.Flow(Format(SDebugStartedPredicateExecutionTemplate, [APredicate.asString]), kContext);
 
-  self.RunPredicate(APredicate);
-
-  for nestedPredicate in APredicate.predicates do
-  begin
-    self.RunPredicate(nestedPredicate, 1 + ALevel,
-      procedure(AResult: TOPPHelpViewPredicateExecutionResult; ALevel: Integer)
+  self.RunConcretePredicate(APredicate, ALevel,
+    procedure(AResult: TOPPHelpViewPredicateExecutionResult; fLevel: Integer)
+    var
+      nestedPredicate: TOPPHelpPredicate;
+    begin
+      if (not(AResult = perSuccess)) then
       begin
-      end);
-  end;
+        eventLogger.Warning('Predicate execution failed', kContext);
+      end;
 
-  eventLogger.Flow(Format(SDebugFinishedPredicateExecutionTemplate, [APredicate.asString]), kEventFlowName);
+      if ((not assigned(APredicate.predicates)) or (APredicate.predicates.Count = 0)) then
+      begin
+        if assigned(completion) then
+          completion(perSuccess, ALevel);
+        exit;
+      end;
 
-  if assigned(completion) then
-    completion(perSuccess, ALevel);
+      for nestedPredicate in APredicate.predicates do
+      begin
+        self.RunPredicate(nestedPredicate, 1 + ALevel,
+          procedure(AResult: TOPPHelpViewPredicateExecutionResult; ALevel: Integer)
+          begin
+
+            eventLogger.Flow(Format(SDebugFinishedPredicateExecutionTemplate, [APredicate.asString]), kContext);
+
+            if assigned(completion) then
+              completion(perSuccess, ALevel);
+
+          end);
+      end;
+    end);
 end;
 
 end.
